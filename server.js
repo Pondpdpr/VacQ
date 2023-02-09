d50f157de9ab5a479c02db7b77ecb22b738145fc
Pondpdpr <pBoonnud@gmail.com>
2023-02-09 15:32:56 +0700

    Connect to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //Route files
 const hospitals = require("./routes/hospitals");
+
+//Connect to database
+connectDB();
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -14,4 +18,11 @@ app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("Server running in ", process.env.NODE_ENV, " mode on port ", PORT));
+const server = app.listen(PORT, console.log("Server running in ", process.env.NODE_ENV, " mode on port ", PORT));
+
+//Handle unhadles promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Clode server & exit process
+  server.close(() => process.exit(1));
+});
